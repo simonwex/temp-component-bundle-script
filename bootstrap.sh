862d59d51033764d7b6519fccfffc08040f7fde5
Simon Wex <simon@simonwex.com>
2014-01-30 00:20:11 -0800

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir component-alternating-gate
+mkdir component-button
+mkdir component-button-with-confirmation
+mkdir component-camera-button
+mkdir component-chat-window
+mkdir component-counter
+mkdir component-fireworks
+mkdir component-header
+mkdir component-image
+mkdir component-metronome
+mkdir component-paypal
+mkdir component-spacer
+mkdir component-splitter
+mkdir component-text-input
+mkdir component-textbox
---
+++ process.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+rm component-*/*
+rm -rf thumbnails/* 2> /dev/null
+
+cp index.html.1 index.html
+
+for COMPONENT in $(ls -l | egrep '^d' | grep 'component' | sed 's/.*component/component/')
+do
+  echo $COMPONENT
+  curl http://mozilla-appmaker.github.io/$COMPONENT/component.html > $COMPONENT/component.html 2> /dev/null
+  curl http://mozilla-appmaker.github.io/$COMPONENT/component.css > $COMPONENT/component.css 2> /dev/null
+  mkdir -p thumbnails/$COMPONENT/
+  curl http://mozilla-appmaker.github.io/$COMPONENT/thumbnail.png > thumbnails/$COMPONENT/thumbnail.png 2> /dev/null
+  # curl http://mozilla-appmaker.github.io/$COMPONENT/thumbnail.png > thumbnails/$COMPONENT.png 2> /dev/null
+  # sed "s/thumbnail\.png/$COMPONENT.png/" $COMPONENT/component.html > ./tmpfile.html
+  # mv tmpfile.html $COMPONENT/component.html
+  echo "<link rel=\"import\" href=\"$COMPONENT/component.html\">"  >> index.html
+done
+
+cat index.html.2 >> index.html
+
+vulcanize index.html
+
+mkdir -p ~/projects/mozilla-appmaker/appmaker/public/bundles/components
+rm -rf ~/projects/mozilla-appmaker/appmaker/public/bundles/components/*
+mv thumbnails/* ~/projects/mozilla-appmaker/appmaker/public/bundles/components/
+cp vulcanized.html ~/projects/mozilla-appmaker/appmaker/public/bundles/components/mozilla-appmaker.html
+
